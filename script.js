cafe72e7b8e37b2747e75547ee08ca88af3f7c49
Preston Patterson <prestonjpatterson@gmail.com>
2020-07-01 21:32:36 -0600

    add the timer not tested and part of the question creater

---
+++ script.js
@@ -0,0 +1,27 @@
+var time = 10
+var QandA = {
+    Q1:["What is a repo?","A) A specific class","B) The name of a bug fix","C) A fancy name for storage", "D)Repo is not a word"]
+    Q2:["How much storage does 1 letter take up?","A)10 bites","B) None","C) 1 bite","D) 1 kilobite"]
+    Q3:["#####","A","B","C","D"]
+    Q4:["#####","A","B","C","D"]
+    Q5:["#####","A","B","C","D"]
+}
+var TimeQ = document.getElementById("timer")
+var startQuiz = document.getElementById("startQuiz")
+var addQuestions = document.
+Function(conter() ,{
+    var: timeI = setInterval(function() {
+        time--;
+        document.getElementById("timer").textContent = time;
+    }),
+}, 1000);
+startQuiz.addEventListener("click", function() {
+    var newQuestions = $("<li>" + QandA.Q1.[i] + "<li>")
+    conter()
+    QandA.Q1.[i]
+    for (var i = 1; i < QandA.Q1.length; i++) {
+        QandA.Q1.[i]
+        
+    }
+    
+})

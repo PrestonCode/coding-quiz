0577a8340e8ffacd4e6b49da05d39539cd1c1143
Preston Patterson <prestonjpatterson@gmail.com>
2020-07-01 23:24:50 -0600

    add question creater does not work.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var time = 10
 var QandA = {
-    Q1:["What is a repo?","A) A specific class","B) The name of a bug fix","C) A fancy name for storage", "D)Repo is not a word"]
-    Q2:["How much storage does 1 letter take up?","A)10 bites","B) None","C) 1 bite","D) 1 kilobite"]
-    Q3:["#####","A","B","C","D"]
-    Q4:["#####","A","B","C","D"]
+    Q1:["What is a repo?","A) A specific class","B) The name of a bug fix","C) A fancy name for storage", "D)Repo is not a word"],
+    Q2:["How much storage does 1 letter take up?","A)10 bites","B) None","C) 1 bite","D) 1 kilobite"],
+    Q3:["#####","A","B","C","D"],
+    Q4:["#####","A","B","C","D"],
     Q5:["#####","A","B","C","D"]
 }
 var TimeQ = document.getElementById("timer")
@@ -16,11 +16,15 @@ Function(conter() ,{
     }),
 }, 1000);
 startQuiz.addEventListener("click", function() {
-    var newQuestions = $("<li>" + QandA.Q1.[i] + "<li>")
+    
     conter()
-    QandA.Q1.[i]
+    $("#question").text("QandA.Q1.[0]");
+    
     for (var i = 1; i < QandA.Q1.length; i++) {
-        QandA.Q1.[i]
+        $("#question").text("QandA.Q" + i + ".[0]");
+        var newQuestion = $("<li>");
+        newQuestion.text(QandA.Q1.[i]);
+        $(".questionArea").prepened(newQuestion)
         
     }
     
